48861d80a255964a7a985d80188bb1b2adfda467
Leonardo de Miranda Borba <lmborba@gmail.com>
2011-05-16 04:22:02 -0300

    Changing logos.

--- db/schema.rb
+++ db/schema.rb
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20110516033035) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "logo"
     t.integer  "position"
   end
 
